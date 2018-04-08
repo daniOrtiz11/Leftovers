6328d2d904cdc67cf677f7c514a6a2b44ee1d924
daniOrtiz11 <dani_glvz@hotmail.es>
2018-04-08 10:57:41 +0200

    Vista Home de la aplicación con diseño inicial

--- leftovers/routes/index.js
+++ leftovers/routes/index.js
@@ -2,11 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 exports.index = function(req, res){
-res.render('index', { title: 'ejs' });};
+res.render('home', { title: 'ejs' });};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
 });
-
 
 module.exports = router;
