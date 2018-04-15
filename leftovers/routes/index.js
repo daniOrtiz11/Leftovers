1316099f964cb8d79907459b33f9b1c758142ad2
daniOrtiz11 <dani_glvz@hotmail.es>
2018-04-15 12:48:03 +0200

    diseño busqueda principal + conectar con home (incompleto)

--- leftovers/routes/index.js
+++ leftovers/routes/index.js
@@ -7,5 +7,26 @@ res.render('home', { title: 'ejs' });};
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
-
+router.get('/image', function(req, res, next){
+	console.log(req.params.imagen);
+	console.log("HOLA DESDE IMAGEN");
+});
+router.get('/principal/:modo?', function(req, res, next) {
+	var modo = req.query.modo;	
+	console.log(modo);
+	res.render('principal', { opcion: modo });
+	//var modo = req.query.modo;
+	/*
+	Si modo = 1, busca con filtro -> principal pestaña recetas
+	busca recetas con filtro....
+	if(modo == 1){
+	
+	}
+	Si modo = 2, buscar sin filtro -> principal pestaña añadir filtros para busqueda
+	busqueda de recetas al azar...
+	else if(modo == 2){
+	
+	}
+	*/
+});
 module.exports = router;
